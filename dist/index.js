487cc9eab3c537557622367ae9cddb16122cb2b2
Federico Builes <febuiles@github.com>
2022-02-04 11:23:49 +0100

    Update action
    Co-authored-by: Christine Nagadya <cnagadya@github.com>

--- dist/index.js
+++ dist/index.js
@@ -1700,14 +1700,7 @@ const wait = __nccwpck_require__(258);
 // most @actions toolkit packages have async methods
 async function run() {
   try {
-    const ms = core.getInput('milliseconds');
-    core.info(`Waiting ${ms} milliseconds ...`);
-
-    core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-    await wait(parseInt(ms));
-    core.info((new Date()).toTimeString());
-
-    core.setOutput('time', new Date().toTimeString());
+    core.info(`Hello from Dependency Graph!`);
   } catch (error) {
     core.setFailed(error.message);
   }
