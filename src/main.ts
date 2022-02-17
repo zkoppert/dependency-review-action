06a57d85d8b185c2054140707d9c338536c8bd26
Federico Builes <febuiles@github.com>
2022-02-17 18:07:07 +0100

    Adding proper output for the fields we have so far.

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,23 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import * as github from '@actions/github'
 
 async function run(): Promise<void> {
+  var payload = {};
+  const context = github.context
+  
+  if (!context.payload.pull_request) {
+    core.setFailed('This action only works on pull requests')
+    return
+  }
+  
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
-
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput('time', new Date().toTimeString())
+    core.info(`Repository\t\t ${context.repo.repo}`)
+    core.info(`Repo Owner\t\t ${context.repo.owner}`)
+    core.info(`Pull Request\t\t ${context.payload.pull_request.number}`)
+    core.info(`Base Branch\t\t ${context.payload.pull_request.base.ref}`)
+    core.info(`Head Branch\t\t ${context.payload.pull_request.head.ref}`)
+    core.info(`Base SHA\t\t ${context.payload.pull_request.base.sha}`)
+    core.info(`Head SHA\t\t ${context.payload.pull_request.head.sha}`)
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
