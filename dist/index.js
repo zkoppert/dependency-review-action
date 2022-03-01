1a4d15aef41d916d820d89a80ae08fc1e918249c
Henri Maurer <hmaurer@github.com>
2022-03-01 09:59:00 +0000

    build dist

--- dist/index.js
+++ dist/index.js
@@ -140,15 +140,15 @@ function run() {
             core.info('This action only works on pull requests');
             return;
         }
-        const repo = context.repo;
-        const pull_request = z
-            .object({
-            number: z.number(),
-            base: z.object({ ref: z.string(), sha: z.string() }),
-            head: z.object({ ref: z.string(), sha: z.string() })
-        })
-            .parse(context.payload.pull_request);
         try {
+            const repo = context.repo;
+            const pull_request = z
+                .object({
+                number: z.number(),
+                base: z.object({ ref: z.string(), sha: z.string() }),
+                head: z.object({ ref: z.string(), sha: z.string() })
+            })
+                .parse(context.payload.pull_request);
             core.info(`Repository\t\t ${repo.repo}`);
             core.info(`Repo Owner\t\t ${repo.owner}`);
             core.info(`Pull Request\t\t ${pull_request.number}`);
@@ -158,6 +158,13 @@ function run() {
             core.info(`Head SHA\t\t ${pull_request.head.sha}`);
             const diff = yield dependencyGraph.compare(pull_request.base.ref, pull_request.head.ref);
             core.info(JSON.stringify(diff, null, 2));
+            const octo = github.getOctokit(core.getInput('repo-token'));
+            const response = yield octo.request('GET /repos/{owner}/{repo}/dependency-graph/{basehead}/diff', {
+                owner: repo.owner,
+                repo: repo.repo,
+                basehead: `${pull_request.base.sha}...${pull_request.head.sha}`
+            });
+            core.info(JSON.stringify(response, null, 2));
         }
         catch (error) {
             if (error instanceof Error)
