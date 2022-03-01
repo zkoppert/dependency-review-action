227432dba47b15dcde240bb0167277848f06e268
Henri Maurer <hmaurer@github.com>
2022-03-01 11:56:53 +0000

    nuke test runs

--- dist/index.js
+++ dist/index.js
@@ -160,8 +160,6 @@ function run() {
                 head: z.object({ ref: z.string(), sha: z.string() })
             })
                 .parse(context.payload.pull_request);
-            console.log(JSON.stringify(pull_request, null, 2));
-            return;
             core.info(`Repository\t\t ${repo.repo}`);
             core.info(`Repo Owner\t\t ${repo.owner}`);
             core.info(`Pull Request\t\t ${pull_request.number}`);
--- src/main.ts
+++ src/main.ts
@@ -22,8 +22,7 @@ async function run(): Promise<void> {
         head: z.object({ref: z.string(), sha: z.string()})
       })
       .parse(context.payload.pull_request)
-    console.log(JSON.stringify(pull_request, null, 2))
-    return
+
     core.info(`Repository\t\t ${repo.repo}`)
     core.info(`Repo Owner\t\t ${repo.owner}`)
     core.info(`Pull Request\t\t ${pull_request.number}`)
